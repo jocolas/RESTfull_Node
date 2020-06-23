8bc6648701aeb2ca07960fa0783640db330cdf02
Joe <weonmart12@gmail.com>
2020-06-22 23:19:10 -0300

    Products

--- app.js
+++ app.js
@@ -2,13 +2,37 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const productRoutes = require('./api/routes/products');
-const orderRoutes = require('./api/routes/orders');
+
+const productRoutes = require('./api/routes/productsRoute');
+const orderRoutes = require('./api/routes/ordersRoute');
+
+mongoose.connect('mongodb://localhost/rest_api',{
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	//useCreateIndex: true
+}).catch(error => console.log(error.reason));
+
+mongoose.connection.on('connected', () => {
+	console.log('Database connection is successful !!!');
+});
 
 app.use(morgan('dev'));
+
 app.use(bodyParser.urlencoded({extended: false}));
+
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*'); //* for all website or https://weonmart.com for a particular or specific web
+	res.header('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+	if (req.method === 'OPTIONS') {
+		res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+		return res.status(200).json({});
+	}
+	next();
+});
 
 //Routes which should handle requests
 app.use('/products', productRoutes);
@@ -28,5 +52,7 @@ app.use((error, req, res, next) => {
 		}
 	});
 });
+
+//app.listen(5000, () => {console.log("server started at http://localhost:3000") });
 
 module.exports = app;
